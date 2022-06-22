f2f0a393068c8314de31ed490d6dd24103df77ad
Terence Ho <>
2022-06-22 21:35:36 +0800

    Remove default implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Hello </p>
-            </header>
-        </div>
-    );
+    return <>Blank</>;
 }
 
 export default App;
