4c463ece8fd9463822eaf35f7858c2e37602ea52
Terence Ho <>
2022-06-25 11:13:08 +0800

    Update text

--- src/common/AboutMeDetails.js
+++ src/common/AboutMeDetails.js
@@ -1,9 +1,9 @@
 const AboutMeDetails = {
-    title: "Hey there! I'm Terence",
+    title: "Hey there! I'm Terence 👋",
     subtitle: "Aspiring Software Engineer. Avid Traveller. Wannabe Climber",
     body: [
         "I'm a final year student from the National University of Singapore (expected graduation end-May 2023).",
-        "As an aspiring Software Engineer, I am looking towards creating scalable Backend systems that can create an impact for my users. I've had experience in Web development, Mobile development (iOS and Android), Database Systems and AI/ML throughout my NUS career.",
+        "As an aspiring Software Engineer, I am looking towards creating scalable Backend systems that can make an impact for my users. I've had experience in Web development, Mobile development (iOS and Android), Database Systems and AI/ML throughout my NUS career.",
     ],
     closing: "You can reach me at ",
     email: "e0388997@u.nus.edu",
