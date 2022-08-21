3608691d5ba830b21fecc04357f303beeaad40e6
Terence Ho <>
2022-08-22 00:57:15 +0800

    Edit resume

--- src/common/Pages.js
+++ src/common/Pages.js
@@ -3,7 +3,7 @@
 const pages = [
     {
         title: "Resume",
-        url: "https://drive.google.com/file/d/1RaZbk03IJMLGzRoICo3zpsUIOFCkHOdH/view?usp=sharing",
+        url: "https://drive.google.com/file/d/1KV7L1I70ZcxfxyKwhGtgCurNyU7Mo8SY/view?usp=sharing",
     },
     {
         title: "GitHub",
